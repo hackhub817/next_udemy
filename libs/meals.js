767afd58cb6b549badefa5f1016921d2a13c2d13
Piyush Gupta <piyush@haltdos.com>
2024-05-14 18:11:14 +0530

    d

--- libs/meals.js
+++ libs/meals.js
@@ -1,5 +1,8 @@
 import sql from "better-sqlite3";
 import { resolve } from "styled-jsx/css";
+import slugify from "slugify";
+import fs from "node:fs";
+import xss from "xss";
 const db = sql("meals.db");
 
 export async function getMeals() {
@@ -10,4 +13,34 @@ export async function getMeals() {
 
 export function getMeal(slug) {
   return db.prepare("SELECT * FROM meals WHERE slug = ? ").get(slug);
+}
+
+export async function saveMeal(meal) {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instructions = xss(meal.instructions);
+
+  const extension = meal.image.name.split(".").pop();
+  const fileName = `${meal.slug}.${extension}`;
+
+  const stream = fs.createReadStream(`public/images/${fileName}`);
+  const bufferedImage = meal.image.arrayBuffer();
+  stream.write(Buffer.from(bufferedImage), (error) => {
+    if (error) {
+      throw new Error("Saving image failed");
+    }
+  });
+
+  meal.image = `/image/${fileName}`;
+
+  db.prepare(
+    `INSERT INTO meals (title,summary,instructions,creator,creator_email,image,slug) VALUES(
+    @title,
+    @summary,
+    @instructions,
+    @creator,
+    @creator_email,
+    @image,
+    @slug,
+  )`
+  ).run(meal);
 }
